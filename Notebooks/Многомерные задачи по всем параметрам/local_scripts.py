86135e173e84c57e81dbad834971f9c49fa3230d
Danis Garipov <danisgrpv@mail.ru>
2022-12-13 00:24:39 +0300

    Gold function

---
+++ Notebooks/Многомерные задачи по всем параметрам/local_scripts.py
@@ -0,0 +1,84 @@
+import time
+import numpy as np
+from GammaRayInteractions.Materials import Material, R
+from NumericalMethods.simpson_rule import simpson_rule
+
+def initialization_brem(material_z, beam_energy, tick=0.01):
+    """
+    Функция принимает материал мишени и энергию налетающего 
+    электронного пучка
+    Возвращаемое значение: генерируемый тормозной спектр
+    """
+    
+    coefficient = Material(material_z).mu()/material_z
+    target = R(material_z, tick*coefficient)
+    bremsstrahlung = target.emitted_bremsstrahlung(beam_energy)
+    
+    return bremsstrahlung
+
+
+def generate_empty_matrix(num_row, num_col):
+    """
+    Функция создает пустую матрицу заданных размеров
+    """
+    empty_matrix = [[i for i in range(num_col)] for j in range(num_row)]
+    return empty_matrix
+
+
+def complete_empty_matrix(empty_matrix, array_1, array_2, func):
+    """
+    Функция заполняет пустую матрицу заданными элементами из массивов
+    array_1, array_2 по правилу, определяемому функцией fucn.
+    Возвращает заполненную матрицу np.ndarray
+    """
+    for row, val_1 in enumerate(array_1):
+        for col, val_2 in enumerate(array_2):
+            empty_matrix[row][col] = func(val_1, val_2)
+            
+    return np.array(empty_matrix)
+
+
+def integrate_the_row(row):
+    """
+    Функция возвращает значение интеграла вычисленное
+    по правилу Симпсона
+    """
+    return simpson_rule(row)
+
+
+def get_integral_signals(bremsstrahlung, K):
+    """
+    Фукнция возвращает результаты измерения спектра
+    измерительной системой K
+    Параметры: 
+    1) измеряемый спектр
+    2) матрица измерительной системы
+    """
+    measurements = K*bremsstrahlung
+    integral_signals = np.apply_along_axis(integrate_the_row, arr=measurements, axis=1)
+    return integral_signals
+
+
+def apply_function_to_matrix(objective_matrix, func, K):
+    """
+    Функция применяет правило, определяемое функцией func к
+    каждому элементу целевой матрицы и создает матрицу возвращаемых
+    значений
+    Параметры:
+    1) целевая матрица
+    2) функция, определяющая возвращаемые значения
+    """
+    num_row = objective_matrix.shape[0]
+    num_col = objective_matrix.shape[1]
+    
+    empty_matrix = [[i for i in range(num_col)] for j in range(num_row)]
+    
+    # время начала
+    start_time = time.time()
+    
+    for row in range(num_row):
+        for col in range(num_col):
+            empty_matrix[row][col] = func(objective_matrix[row][col], K)
+            print(f'Выполнено {col+1} из {num_col}. Время: {round(time.time()-start_time, 4)} секунды')
+        print(f'ВЫПОЛНЕНО {row+1} из {num_row} СТОЛБЦОВ. Время: {round(time.time()-start_time, 4)} секунды') 
+    return np.array(empty_matrix)
